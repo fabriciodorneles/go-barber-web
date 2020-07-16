9b379044897aed5d1b4de2f8be77dff9d7db7c45
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-07-16 11:53:08 -0300

    no message

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <h1>Você Está logado no Sistema</h1>
+        </>
+    );
+};
 
 export default Dashboard;
