8b982e1f5bdddfe3ec3fee327854405c198aa2e5
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-08-12 21:58:38 -0300

    front appointments

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    NextAppointment,
+    Section,
+    Appointment,
+    Calendar,
+} from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const { signOut, user } = useAuth();
     return (
-        <>
-            <h1>Dashboard</h1>
-            <h1>Você Está logado no Sistema</h1>
-        </>
+        <Container>
+            <Header>
+                <HeaderContent>
+                    <img src={logoImg} alt="GoBarber" />
+
+                    <Profile>
+                        <img src={user.avatar_url} alt={user.name} />
+                        <div>
+                            <span>Seja bem-vindo,</span>
+                            <strong>{user.name}</strong>
+                        </div>
+                    </Profile>
+                    <button type="button" onClick={signOut}>
+                        <FiPower />
+                    </button>
+                </HeaderContent>
+            </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários Agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 6</span>
+                        <span>Segunda-feira</span>
+                    </p>
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img
+                                src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4"
+                                alt="Gean Lucas"
+                            />
+                            <strong>Gean Lucas</strong>
+                            <span>
+                                <FiClock /> 08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+
+                    <Section>
+                        <strong>Manhã</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img
+                                    src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4"
+                                    alt="Gean Lucas"
+                                />
+                                <strong>Gean Lucas</strong>
+                            </div>
+                        </Appointment>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img
+                                    src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4"
+                                    alt="Gean Lucas"
+                                />
+                                <strong>Gean Lucas</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                    <Section>
+                        <strong>Tarde</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img
+                                    src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4"
+                                    alt="Gean Lucas"
+                                />
+                                <strong>Gean Lucas</strong>
+                            </div>
+                        </Appointment>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img
+                                    src="https://avatars0.githubusercontent.com/u/24641532?s=460&u=2fd2df1fae9711cebf45c19d7c10ad8d09ee12b7&v=4"
+                                    alt="Gean Lucas"
+                                />
+                                <strong>Gean Lucas</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                </Schedule>
+                <Calendar />
+            </Content>
+        </Container>
     );
 };
 
